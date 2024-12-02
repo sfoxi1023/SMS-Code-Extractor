9064e6c16c60ec9522b2477f08dcab68a3a14b75
sfoxi1023 <sfoxi1023@gmail.com>
2024-12-02 18:50:10 +0300

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,26 @@
+@file:Suppress("UnstableApiUsage", "UnstableApiUsage", "UnstableApiUsage", "RedundantSuppression")
+
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "CodeExtractor"
+include(":app")
+ 
